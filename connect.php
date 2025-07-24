462c93c1dced071223572a881fefbb1df8b6753c
YukikungEiEi <kimizakiyama@gmail.com>
2025-07-24 12:18:11 +0700

    ez

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "itinwza007x";
+
+    $con = mysqli_connect ($host, $user, $pass,$dbname);
+
+    // Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+
+?>  
---
+++ logout.php
@@ -0,0 +1,5 @@
+#file logout.php
+<?php
+    session_start();
+    session_destroy();
+    header('Location: login.php');
